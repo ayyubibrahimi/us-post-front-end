b9b8fea72691036b4eed9db8330e9d6e44e625e9
olive <olivealavine@gmail.com>
2024-09-16 13:22:51 -0500

    hide ga id

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from "next/app";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import "../styles/global.css";
 
+const gaId = process.env.NEXT_PUBLIC_GA_ID as string;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <GoogleAnalytics gaId="G-SQ9DN4BSF4" />
+      <GoogleAnalytics gaId={gaId} />
     </>
   );
 }
